bf558add0b5244f72afc70b471a3a3a0aa416546
lyloou <lyloou@qq.com>
2015-05-05 08:11:16 +0800

    准备添加授权信息(还没完成)

--- app/src/main/java/com/lyloou/weibo/view/LoginActivity.java
+++ app/src/main/java/com/lyloou/weibo/view/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.lyloou.weibo.view;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -9,11 +8,16 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 import com.lyloou.weibo.R;
+import com.lyloou.weibo.app.Constants;
+import com.sina.weibo.sdk.auth.AuthInfo;
+import com.sina.weibo.sdk.auth.WeiboAuthListener;
+import com.sina.weibo.sdk.auth.sso.SsoHandler;
+import com.sina.weibo.sdk.exception.WeiboException;
 
-public class LoginActivity extends Activity implements IWeiboActivity {
-
+public class LoginActivity extends Activity implements IWeiboActivity, OnClickListener {
+	private AuthInfo mAuthInfo;
+	private SsoHandler mSsoHandler;
 	protected static final String TAG = "Weibo";
-
 	Handler handler = new Handler() {
 		public void handleMessage(android.os.Message msg) {
 			if (msg.what == 1) {
@@ -25,24 +29,15 @@ public class LoginActivity extends Activity implements IWeiboActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-		Button loginBtn = (Button) findViewById(R.id.id_login_btn);
-		Button addUserBtn = (Button) findViewById(R.id.id_add_user_btn);
+		Button loginBtn = (Button) findViewById(R.id.id_auth_login_btn);
+		Button addUserBtn = (Button) findViewById(R.id.id_auth_add_user_btn);
 
-		loginBtn.setOnClickListener(new OnClickListener() {
+		mAuthInfo = new AuthInfo(LoginActivity.this, Constants. APP_KEY,
+				Constants. REDIRECT_URL, Constants. SCOPE);
+		mSsoHandler = new SsoHandler(LoginActivity.this, mAuthInfo);
 
-			@Override
-			public void onClick(View v) {
-
-			}
-		});
-		addUserBtn.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(LoginActivity.this,AuthActivity.class);
-				startActivity(intent);
-			}
-		});
+		loginBtn.setOnClickListener(this);
+		addUserBtn.setOnClickListener(this);
 
 	}
 
@@ -53,4 +48,32 @@ public class LoginActivity extends Activity implements IWeiboActivity {
 	public void refresh(Object... obj) {
 	}
 
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()){
+			case R.id.id_auth_add_user_btn:
+				mSsoHandler.authorize(new AuthListener());
+				break;
+			case R.id.id_auth_login_btn:
+				break;
+		}
+	}
+
+	private class AuthListener implements WeiboAuthListener {
+		@Override
+		public void onComplete(Bundle bundle) {
+
+		}
+
+		@Override
+		public void onWeiboException(WeiboException e) {
+
+		}
+
+		@Override
+		public void onCancel() {
+
+		}
+	}
 }
